7c3880514c063da14a00fcdb71bd50a7eaf023f5
Syafiq Basri <syafiqbasri@pipeline-network.com>
2020-06-11 03:43:39 +0800

    feedbacks

--- feedbacks/models.py
+++ feedbacks/models.py
@@ -16,10 +16,10 @@ from users.models import (
 class Complaint(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=100, default='NA')
-    email = models.CharField(max_length=100, default='NA')
-    phone = models.CharField(max_length=100, default='NA')
-    complaint = models.TextField(default='NA')
+    name = models.CharField(max_length=100, null=True)
+    email = models.CharField(max_length=100, null=True)
+    phone = models.CharField(max_length=100, null=True)
+    complaint = models.TextField(null=True)
     complaint_to = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True, related_name='complaint_to')
 
     COMPLAINER_TYPE = [
@@ -33,7 +33,7 @@ class Complaint(models.Model):
         default='PT'
     )
 
-    location = models.CharField(max_length=255, default='NA')
+    location = models.CharField(max_length=255, null=True)
     date = models.DateTimeField(blank=True)
 
     COMPLAINT_CLASS = [
@@ -90,7 +90,7 @@ class Complaint(models.Model):
     complaint_category = models.CharField(
         max_length=2,
         choices=COMPLAINT_CATEGORY,
-        default='NA'
+        null=True
     )
     supporting_docs = models.FileField(null=True, upload_to=PathAndRename('docs'))
 
@@ -118,7 +118,7 @@ class Complaint(models.Model):
 class SurveyQuestion(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    question = models.CharField(max_length=100, default='NA')
+    question = models.CharField(max_length=100, null=True)
     
     CATEGORY = [
         ('IN', 'Internal'),
@@ -128,7 +128,7 @@ class SurveyQuestion(models.Model):
     category = models.CharField(
         max_length=2,
         choices=CATEGORY,
-        default='NA'
+        null=True
     )
 
     SURVEY_TYPE = [
@@ -143,7 +143,7 @@ class SurveyQuestion(models.Model):
     survey_type = models.CharField(
         max_length=2,
         choices=SURVEY_TYPE,
-        default='NA'
+        null=True
     )
 
     created_date = models.DateTimeField(auto_now_add=True)
@@ -159,7 +159,7 @@ class SurveyQuestion(models.Model):
 class SurveyAnswer(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    answer = models.CharField(max_length=100, default='NA')
+    answer = models.CharField(max_length=100, null=True)
     question = models.ForeignKey(SurveyQuestion, on_delete=models.CASCADE)
     by = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True, related_name='survey_answer_by')
 
@@ -171,4 +171,67 @@ class SurveyAnswer(models.Model):
     
     def __str__(self):
         return self.question
+
+
+class Feedback(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    is_patient = models.BooleanField(default=True)
+    name = models.CharField(max_length=100, null=True)
+    mrn = models.CharField(max_length=100, null=True)
+    phone = models.CharField(max_length=100, null=True)
+    email = models.CharField(max_length=100, null=True)
+
+    PATIENT_TYPE = [
+        ('IN', 'Inpatient'),
+        ('OUT', 'Outpatient')
+    ]
+    patient_type = models.CharField(
+        max_length=2,
+        choices=PATIENT_TYPE,
+        default='IN'
+    )
+
+    FEEDBACK_TYPE = [
+        ('01', 'How get to know IJN'),
+        ('02', 'Access and waiting time'),
+        ('03', 'Information and communication'),
+        ('04', 'Patient and family rights'),
+        ('05', 'Confidence in health professionals'),
+        ('06', 'Safety of environment'),
+        ('07', 'Hygience and cleaniness'),
+        ('08', 'Food and beverages'),
+        ('09', 'Meet expectations?'),
+        ('10', 'Recommend IJN'),
+        ('11', 'Best experience'),
+        ('12', 'Suggestion for improvement')
+    ]
+    feedback_type = models.CharField(
+        max_length=2,
+        choices=FEEDBACK_TYPE,
+        default='01'
+    )
+    feedback = models.CharField(max_length=100, null=True)
+
+    WAITING_TYPE = [
+        ('AD', 'Admission'),
+        ('DC', 'Discharge')
+    ]
+    waiting_type = models.CharField(
+        max_length=2,
+        choices=WAITING_TYPE,
+        default='AD'
+    )
+
+    is_acceptable = models.BooleanField(default=True)
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    class meta:
+        ordering = ['question']
+    
+    def __str__(self):
+        return self.question
+
 
